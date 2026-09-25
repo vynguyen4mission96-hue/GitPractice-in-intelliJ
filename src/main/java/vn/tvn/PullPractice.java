432a6b0616811809a92e89bcf3e8cd546e474e62
Vy-Nguyen-96 <vynguyen4mission96@gmail.com>
2026-09-25 16:17:59 +0700

    Update PullPractice.java

--- src/main/java/vn/tvn/PullPractice.java
+++ src/main/java/vn/tvn/PullPractice.java
@@ -1,2 +1,11 @@
 //lần 1
 //pull code to local 
+
+package vn.tvn;
+public class PullPractice {
+
+    public static void main(String[] args) {
+
+        System.out.println("This file was created on GitHub.");
+    }
+}
