5878eaab729261bb8bf425563e72091d4893f8bf
THIS PC <vynguyen4mission96@gmail.com>
2026-09-25 16:09:48 +0700

    Update hello_2 and add hello_3

--- src/main/java/vn/tvn/hello_2.java
+++ src/main/java/vn/tvn/hello_2.java
@@ -1,6 +1,9 @@
 package vn.tvn;
 
 public class hello_2 {
+    //lần 1:
     //hello git (2)
     //push code to github
+    //lần 2
+    // This is a new comment for Git practice.
 }
---
+++ src/main/java/vn/tvn/hello_3.java
@@ -0,0 +1,9 @@
+package vn.tvn;
+
+public class hello_3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git - version 3");
+    }
+}
