0f1777b5b5ee75a83f832ec64ca55a472f984a08
Vy-Nguyen-96 <vynguyen4mission96@gmail.com>
2026-09-25 16:26:44 +0700

    Update hello_2.java

--- src/main/java/vn/tvn/hello_2.java
+++ src/main/java/vn/tvn/hello_2.java
@@ -6,4 +6,10 @@ public class hello_2 {
     //push code to github
     //lần 2
     // This is a new comment for Git practice.
+
+    //pull lần 2
+      public static void main(String[] args) {
+        // This comment was added directly on GitHub for pull practice.
+        System.out.println("Hello Git");
+    }
 }
